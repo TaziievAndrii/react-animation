8916a4dfc6f94f33cb0870a4a9f4a195848894cc
Taziiev Andrii <andrewtaziiev@gmail.com>
2024-04-12 13:54:47 +0300

    add banner

--- src/components/Home/MainBanner.jsx
+++ src/components/Home/MainBanner.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 
 import Section from "../Section/Section";
 
-import video from "../../images/videoplayback.webm"
+import video from "../../images/video.mp4"
 
-const MainBanner = () =>  (
-        <Section>
-            <div className="container">
-                <h1 style={{fontSize:0,lineHeight:0}}>Evanescence</h1>
-                <div className="banner">
-                    <video className={`banner-video`} width={1000} height={200} autoPlay loop muted>
-                        <source src={video} type="video/webm" />
-                    </video>
-                </div>
+const MainBanner = () => (
+    <Section>
+        <div className="container">
+            <h1 style={{fontSize: 0, lineHeight: 0}}>Evanescence</h1>
+            <div className="banner">
+                <video className={`banner-video`} width={1000} height='auto' autoPlay loop muted>
+                    <source src={video} type="video/mp4"/>
+                </video>
             </div>
-        </Section>
-    );
+        </div>
+    </Section>
+);
 
 
 export default MainBanner;
