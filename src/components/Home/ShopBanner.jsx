c84ad4d10357f7292ff77eca4ed89ead77d6df2b
Taziiev Andrii <andrewtaziiev@gmail.com>
2024-07-01 21:16:25 +0300

    fixed banner

--- src/components/Home/ShopBanner.jsx
+++ src/components/Home/ShopBanner.jsx
@@ -4,20 +4,41 @@ import ScrollAnimation from 'react-animate-on-scroll';
 
 import Section from "../Section/Section";
 
+import logo from '../../images/symbol.svg'
+import bannerImg from '../../images/Evanescence__logo.svg'
+
 const ShopBanner = () => {
     return (
         <Section className={`shop-banner__section`}>
-            <div className="ontainer">
+            <div className="container">
                 <div className="shop-banner__banner">
-                    <Link to={`/shop`}>
+                    <Link to={`/shop`} className={`shop-banner`}>
                         <ScrollAnimation className={`shop-banner__text`} animateIn='fadeInLeft' animateOut='fadeOutLeft'
                                          duration={3} offset={0}>
                             <p className="shop-banner__subtitle">
                                 New merchandise
                             </p>
                             <p className="shop-banner__title">
-                                Evanescence shop
+                                Shop
                             </p>
+                        </ScrollAnimation>
+                        <ScrollAnimation
+                            className={`shop-banner__logo`}
+                            animateIn='fadeInRight'
+                            animateOut='fadeOutRight'
+                            duration={3}
+                            offset={0}
+                        >
+                            <img src={logo} alt="Evanescenceshop"/>
+                        </ScrollAnimation>
+                        <ScrollAnimation
+                            className={`shop-banner__image`}
+                            animateIn='fadeInRight'
+                            animateOut='fadeOutRight'
+                            duration={3}
+                            offset={0}
+                        >
+                            <img src={bannerImg} alt="banner"/>
                         </ScrollAnimation>
                     </Link>
                 </div>
